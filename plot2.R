d8df2db5a11189a7751ab5c86483bba0124cb691
raynus <raynus.blueray@gmail.com>
2016-03-05 09:05:51 +0700

    Added files via upload

---
+++ plot2.R
@@ -0,0 +1,19 @@
+#plot2
+
+setwd("C:/Users/raynus4/Desktop/HI_curriculum/course/r_explore")
+data<-read.table("household_power_consumption.txt",sep=";",header = TRUE)
+data$Date<-as.character(data$Date)
+data$Date_new<-as.Date(data$Date,format="%d/%m/%Y")
+data$Time_new<-strptime(data$Time,format="%H:%M:%S")
+data$Time_new<-format(data$Time_new,format="%H:%M:%S")
+
+data$Date_time<-strptime(paste(data$Time_new,data$Date_new),format ="%H:%M:%S %Y-%m-%d" )
+data$wk_data<-wday(data$Date_new,label = TRUE)
+
+
+sub_two_days<-subset(data,Date_new>=as.Date("2007-01-04") & Date_new <=("2007-01-5"))
+
+png(file="plo2.png",width=800,height=600)
+with(sub_two_days,plot(Date_time,Global_active_power,type="l",xlab="Week day",ylab="Global Active Power (kilowatts"))
+dev.off()
+
